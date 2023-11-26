601a6f9d5ae0ac1def327077caa0083de3b83f6d
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2023-11-26 18:46:31 +0530

    react home

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,11 +4,14 @@
 import Youtube from "./Youtube"
 
 function App() {
+  const username = 'Sahil'
+  // variable going inside the return value should already be evaluated.
+  // we cannot write an statement there
   return (
     <>
-      <p> in vite app </p>
+      <p> in vite app {2+2} </p>
 
-      <p> in vite app </p>
+      <p> in vite app {username} </p>
       <Youtube />
     </>
   )
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp(){
+    return(
+        <div>
+            <h1>
+                Custome React App
+            </h1>
+        </div>
+    )
+}
+
+
+const AnotherElement = (
+    <a href='http://google.com' target='_blank'>Visit Google</a>
+)
+
+// to convert an object into react element
+const areactElement = React.createElement(
+    // order is very importanat
+    'a',  // first parameter should be an element
+    {href:'http://google.com', target:'_blank'}, // second should be attributes
+    'click to visit google' // third should be inner HTML or child elementh
+    // username // react adds fourth variable as evaluated variable
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <App />
+
+    // MyApp() // can be run like this as well
+    //  AnotherElement
+    // areactElement
+
 
 )
